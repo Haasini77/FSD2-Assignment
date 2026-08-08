7c13e3641bce755d716da43eb36fc8d23f62ee76
Haasini77 <haasiniobilisetty@gmail.com>
2026-08-08 18:27:33 +0530

    Delete bintoddec.ts

--- bintoddec.ts
+++
@@ -1,9 +0,0 @@
-let secondValue: string = "1011";
-let d: number = 0;
-for (let i = 0; i < secondValue.length; i++) {
-    d = d * 2;
-    if (secondValue[i] === "1") {
-        d++;
-    }
-}
-console.log(d);
